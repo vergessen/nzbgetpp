0a4ef677e6f7bc19e5972d5eee4a669008e62de3
vergessen <vergessen@gmail.com>
2014-02-28 20:10:44 -0600

    updated to work with category.

--- unzip.py
+++ unzip.py
@@ -4,15 +4,15 @@ import sys
 import zipfile
 
 ext = os.path.splitext(os.environ['NZBNP_FILENAME'])[1]
-
+cat = os.environ['NZBNP_CATEGORY']
 if ext.lower() == '.zip':
     zipf = zipfile.ZipFile(os.environ['NZBNP_FILENAME'], mode='r')
     zips = zipf.infolist()
     for sf in zips:
-         sf.filename = 'cat.'+ os.environ['NZBNP_CATEGORY'] + '.' + sf.filename
-         zipf.extract(sf,os.environ['NZBNP_DIRECTORY'])
-else:
-    checkcat = os.environ['NZBNP_NZBNAME'].split('.',2)
-    if checkcat[0] == 'cat':
-       print("[NZB] NZBNAME=%s" % checkcat[2])
-       print("[NZB] CATEGORY=%s" % checkcat[1])
+         if cat != "":
+             zipf.extract(sf,os.environ['NZBNP_DIRECTORY'] + os.path.sep + os.environ['NZBNP_CATEGORY'])
+         else:
+             zipf.extract(sf,os.environ['NZBNP_DIRECTORY'])
+
+#file is there but has to be cleared on next pass?
+#    os.unlink(os.environ['NZBNP_FILENAME'] + '.processed')
