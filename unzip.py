b619f744027e2a2c217733d76e49d6832e19001f
vergessen <vergessen@gmail.com>
2014-06-07 19:46:25 -0500

    order add error checking for split, could fail on files without a ext

--- unzip.py
+++ unzip.py
@@ -16,10 +16,11 @@ import os
 import sys
 import zipfile
 
-ext = os.path.splitext(os.environ['NZBNP_FILENAME'])[1]
+filename = os.environ['NZBNP_FILENAME']
+ext = os.path.splitext(filename)[1]
 cat = os.environ['NZBNP_CATEGORY']
 dir = os.environ['NZBNP_DIRECTORY']
-filename = os.environ['NZBNP_FILENAME']
+
 if ext.lower() == '.zip':
     zipf = zipfile.ZipFile(filename, mode='r')
     if cat != "":
